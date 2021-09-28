ab85817ff31cb8f2f3d81f2545062d9b45a4ab4a
Михаил Ященко <chemist138@mail.ru>
2021-09-28 20:26:35 +0300

    first commit for my social network project

---
+++ src/social_network/project_overview.md
@@ -0,0 +1,10 @@
+1. profiles
+    - Profile
+    - Relationship model
+
+2. posts
+    - Post
+    - Comment
+    - Like
+
+3. authentification - allauth
---
+++ src/static_project/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#modal-btn').click(function(){
+        console.log('js works')
+        $('.ui.modal')
+        .modal('show')
+        ;
+    })
+})
